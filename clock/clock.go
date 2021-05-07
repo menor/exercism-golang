49d50060f50b3229904afa49c5181b5961c464f6
menor <josemenor@gmail.com>
2021-05-07 09:43:13 +0200

    Improve Clock exercise with comments

--- clock/clock.go
+++ clock/clock.go
@@ -54,20 +54,31 @@ func (c Clock) Subtract(minutes int) Clock {
 }
 
 func format(h, m int) (hours, minutes int) {
+	// if we have more than 60 minutes we split that
+	// into hour and minutes
 	if m >= 60 || m <= -60 {
 		h = h + m/60
 		m = m % 60
 	}
 
+	// If we have negative minutes means we need to:
+	// - Substract at least one hour
+	// - Substract the remaining minutes from 60
+	// i.e. 12:00 - 15min = 11:45
 	if m < 0 {
 		h = h - (1 + m/60)
-		m = 60 + m
+		m = 60 + m%60
 	}
 
+	// If we have more than 24 hours
+	// A clock only counts until 24 hours, so we keep the
+	// remainder of dividing the hours by 24
 	if h >= 24 || h <= -24 {
 		h = h % 24
 	}
 
+	// If we have negative hours means we need to substract them
+	// from a complete clock
 	if h < 0 {
 		h = 24 + h%24
 	}
