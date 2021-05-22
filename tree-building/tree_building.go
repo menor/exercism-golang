01ea5f4cb2444dcdd168bd4e6d86d6af0de799b3
menor <josemenor@gmail.com>
2021-05-22 09:26:17 +0200

    add tree-building solution

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -1,1 +1,43 @@
 package tree
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Record struct {
+	ID     int
+	Parent int
+}
+
+type Node struct {
+	ID       int
+	Children []*Node
+}
+
+const rootId = 0
+
+func Build(records []Record) (*Node, error) {
+
+	sort.Slice(records, func(i, j int) bool { return records[i].ID < records[j].ID })
+
+	tree := make(map[int]*Node)
+
+	for i, r := range records {
+		noRootNode := i != rootId
+		rootNodeHasParent := r.Parent > i
+		noPreviousNode := i != r.ID
+		nodeIsItsOwnParent := (noRootNode && r.Parent == i)
+
+		if noPreviousNode || rootNodeHasParent || nodeIsItsOwnParent {
+			return nil, fmt.Errorf("invalid record: %+v", r)
+		}
+		n := &Node{ID: r.ID}
+		if r.ID > 0 {
+			tree[r.Parent].Children = append(tree[r.Parent].Children, n)
+		}
+		tree[i] = n
+	}
+
+	return tree[rootId], nil
+}
